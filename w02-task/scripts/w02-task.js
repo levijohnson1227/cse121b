8643c7052a713d35014672f65a49c15702e6ca19
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-09-23 14:53:40 -0600

    updated foods

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -13,13 +13,13 @@ let profilePicture = 'images/templePic.jpg';
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('img[src="images/templePic.jpg"]');
+const imageElement = document.querySelector('img[src="images/placeholder.png"]');
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
-foodElement.innerHTML = favoriteFoods;
+foodElement.textContent = favoriteFoods;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt',`<strong>'Prolie image of ${fullName}'</strong>`);
 
