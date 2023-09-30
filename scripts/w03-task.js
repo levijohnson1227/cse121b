2d8d7affe3407645d04815cf49169cea94853131
levijohnson1227 <lw.johnson.1227@gmail.com>
2023-09-30 11:08:49 -0600

    update

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -9,9 +9,9 @@ function add (number1, number2)
 
 function addNumbers ()
 {
-    let addNumber1 = Number(document.querySelector('#add1').value);
-    let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum') = add(addNumber1, addNumber2);
+    let addNumber1 = Number(document.querySelector('#add1').value)
+    let addNumber2 = Number(document.querySelector('#add2').value)
+    document.querySelector('#sum') = add(addNumber1, addNumber2)
 
 }
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
@@ -24,9 +24,9 @@ const subtract = function (number1, number2)
 
 const subtractNumbers = function()
 {
-    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
-    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-    document.querySelector('#difference') = add(subtractNumber1, subtractNumber2);
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value)
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value)
+    document.querySelector('#difference') = add(subtractNumber1, subtractNumber2)
 }
 document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
@@ -35,9 +35,9 @@ const multiply = (number1, number2) => number1 * number2;
 
 const multiplyNumbers = () => 
 {
-    let multiplyNumber1 = Number(document.querySelector('#multiply1').value);
-    let multiplyNumber2 = Number(document.querySelector('#multiply2').value);
-    document.querySelector('#product') = add(multiplyNumber1, multiplyNumber2);
+    let multiplyNumber1 = Number(document.querySelector('#multiply1').value)
+    let multiplyNumber2 = Number(document.querySelector('#multiply2').value)
+    document.querySelector('#product') = add(multiplyNumber1, multiplyNumber2)
 }
 document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
@@ -46,9 +46,9 @@ const divide = (number1, number2) => number1 / number2;
 
 function divideNumbers ()
 {
-    let divideNumber1 = Number(document.querySelector('#divide1').value);
-    let divideNumber2 = Number(document.querySelector('#divide2').value);
-    document.querySelector('#quotient') = add(divideNumber1, divideNumber2);
+    let divideNumber1 = Number(document.querySelector('#divide1').value)
+    let divideNumber2 = Number(document.querySelector('#divide2').value)
+    document.querySelector('#quotient') = add(divideNumber1, divideNumber2)
 
 }
 document.querySelector('#divideNumbers').addEventListener('click', Numbers);
